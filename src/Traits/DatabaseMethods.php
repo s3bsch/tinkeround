1117e934c0e5eefbb7ecde7f20a450d891c6fe24
Sebastian Huber <sh@hejfish.com>
2019-12-07 07:55:36 +0100

    Provide logging of database queries if enabled

--- src/Traits/DatabaseMethods.php
+++ src/Traits/DatabaseMethods.php
@@ -10,6 +10,9 @@ use Illuminate\Support\Facades\DB;
  */
 trait DatabaseMethods
 {
+    /** @var bool If true, executed database queries are logged */
+    protected $logQueries = false;
+
     /** @var int Query count since last reset */
     protected $queryCount = 0;
 
@@ -21,6 +24,35 @@ trait DatabaseMethods
 
     /** @var float Overall query time during tinkeround session in milliseconds */
     protected $queryTimeTotal = 0.0;
+
+    /**
+     * Disable logging of executed database queries.
+     */
+    public function disableQueryLogging(): void
+    {
+        $this->logQueries = false;
+    }
+
+    /**
+     * Enable logging of executed database queries.
+     */
+    public function enableQueryLogging(): void
+    {
+        $this->logQueries = true;
+    }
+
+    /**
+     * Log executed database query in case query logging is enabled.
+     * Additionally log bindings in case logging of bindings is enabled.
+     *
+     * @param QueryExecuted $query Executed database query
+     */
+    public function logQuery(QueryExecuted $query): void
+    {
+        if ($this->logQueries) {
+            $this->log($query->sql);
+        }
+    }
 
     /**
      * @return int Total count of queries made during tinkeround session
@@ -45,6 +77,8 @@ trait DatabaseMethods
 
         $this->queryTime += $query->time;
         $this->queryTimeTotal += $query->time;
+
+        $this->logQuery($query);
     }
 
     protected function registerQueryListener(): void
---
+++ tests/DatabaseMethods/LogQueryTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Tinkeround\Tests\DatabaseMethods;
+
+use Illuminate\Database\Events\QueryExecuted;
+use Tinkeround\Tests\DumpCollector;
+use Tinkeround\Tests\TestCase;
+use Tinkeround\Tinkeround;
+
+/**
+ * Test for the `logQuery()` method.
+ */
+class LogQueryTest extends TestCase
+{
+    /** @var Tinkeround */
+    private $testy;
+
+    function setUp(): void
+    {
+        $this->testy = $this->createTinkeroundMock();
+    }
+
+    function test_it_does_not_log_query_by_default()
+    {
+        $collector = new DumpCollector();
+
+        $query = $this->createFakeQuery('fake sql');
+        $this->testy->logQuery($query);
+
+        $this->assertEquals(0, $collector->dumpCount());
+    }
+
+    function test_it_logs_query_in_case_logging_is_enabled()
+    {
+        $collector = new DumpCollector();
+        $this->testy->enableQueryLogging();
+
+        $query = $this->createFakeQuery('fake sql');
+        $this->testy->logQuery($query);
+
+        $this->assertEquals(1, $collector->dumpCount());
+        $this->assertEquals('fake sql', $collector->shiftDump());
+    }
+
+    function test_it_does_not_log_queries_after_disabling_logging()
+    {
+        $collector = new DumpCollector();
+        $this->testy->enableQueryLogging();
+
+        $query = $this->createFakeQuery('fake sql 1');
+        $this->testy->logQuery($query);
+
+        $this->testy->disableQueryLogging();
+
+        $query = $this->createFakeQuery('fake sql 2');
+        $this->testy->logQuery($query);
+
+        $this->assertEquals(1, $collector->dumpCount());
+        $this->assertEquals('fake sql 1', $collector->shiftDump());
+    }
+
+    /** @noinspection PhpUndefinedFieldInspection */
+    private function createFakeQuery(string $sql, array $bindings = [])
+    {
+        $fakeQuery = $this->createMock(QueryExecuted::class);
+        $fakeQuery->sql = $sql;
+        $fakeQuery->bindings = $bindings;
+
+        /** @var QueryExecuted $fakeQuery */
+        return $fakeQuery;
+    }
+}
